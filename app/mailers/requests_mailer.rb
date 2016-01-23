5200998e304d921d606063df7bf7a126a61dd56c
Andriy Borodiychuk <ab@markusweb.com>
2016-01-23 23:57:14 +0100

    Request email fix

--- app/mailers/requests_mailer.rb
+++ app/mailers/requests_mailer.rb
@@ -2,12 +2,11 @@ class RequestsMailer < ActionMailer::Base
   default :from => "Kernl <today@kernl.rocks>", :bcc => "kernl.bcc@markusweb.com, kernl.bcc@fetz.cc"
 
   def inquiry data
-    @account         = data[:account]
     @project         = data[:project]
     @product         = data[:product]
     @product_variant = data[:product_variant]
     @message         = data[:message]
-    mail :subject => "#{@project.name} Anfrage", :to => @account.email
+    mail :subject => "#{@project.name} Anfrage", :to => @project.account.email
   end
 
 end
