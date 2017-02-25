eefdde023b4716fbc9e560dab605ac0defeabbd6
Andriy Borodiychuk <ab@markusweb.com>
2017-02-25 12:33:26 +0100

    Just adding a memo to comment not to forget

--- app/workers/touching_worker.rb
+++ app/workers/touching_worker.rb
@@ -6,6 +6,7 @@ class TouchingWorker
   def perform class_name, id
     logger.debug "* Performing async touch of #{class_name} ##{id}"
     object = class_name.constantize.find_by_id id
+    # TODO: We can touch it only it has been touched at least couple seconds ago to avoid senseless updates
     object.touch if object
   end
 
